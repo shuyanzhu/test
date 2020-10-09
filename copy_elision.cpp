9227644e9996f11adb55d3116932883e3fd55ebe
eureka <1294206499@qq.com>
2020-10-09 16:39:07 +0800

    一般提交

--- copy_elision.cpp
+++ copy_elision.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 struct X {
-    X()  { std::cout << "default constructor" << std::endl; }
+    X()  { std::cout << "default constructor in "<< this << std::endl; }
     ~X() { std::cout << "destructor in "       <<  this << std::endl; }
 
-    X(const X&)            { std::cout << "copy constructor\n";         }
-    X& operator=(const X&) { std::cout << "copy assignment operator\n"; }
+    X(const X&)            { std::cout << "copy constructor in " << this << std::endl;         }
+    X& operator=(const X&) { std::cout << "copy assignment operator\n"; return *this;}
 };
 
 X f() {
-    // in c++11, -fno-copy-elistion, def->copy->des->copy(tmp)->des(x)->copy(a)->des(tmp)->des(a)
+    // in c++11, -fno-copy-elistion, def(tmp in f)->copy(x)->des(tmp in f)->copy(tmp in main)->des(x)->copy(a)->des(tmp in main)->des(a)
     // in C++17, -fno-copy-elision, def->copy->des->des
     // in c++17/c++11/c++03, def->des 
    X x = X();
@@ -22,7 +22,26 @@ X f() {
 }
 
 int main() {
-    X &&a = f();
-    std::cout << "Debug" << std::endl;
+    std::cout << "sizeof(X) = " << sizeof(X) << std::endl;
+    // &a0:         0x7ffffffedc28
+    // tmp in main: 0x7ffffffedc20
+    // x:           0x7ffffffedbc0 + 96
+    // tmp in f:    0x7ffffffedbb8 + 8
+    X a0 = f();
+    std::cout << std::endl;
+    // &a1:         0x7ffffffedc00
+    // x:           0x7ffffffedbc0 + 64
+    // tmp in f:    0x7ffffffedbb8 + 8
+    const X &a1 = f();
+    std::cout << std::endl;
+    // // &a2:         0x7ffffffedbf0 
+    // // x:           0x7ffffffedbc0 + 48
+    // // tmp in f:    0x7ffffffedbb8 + 8
+    X &&a2 = f();
+    std::cout << std::endl;
+    // std::cout << "Debug" << std::endl;
+    // destroy a2
+    // destroy a1
+    // destroy a0
     return 0;
 }
