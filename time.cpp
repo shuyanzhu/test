f27b23e2ccc7340a53419c473f16afc0c6a8bd48
eureka <1294206499@qq.com>
2021-07-07 10:49:16 +0800

    测试了gettime系统调用

--- time.cpp
+++ time.cpp
@@ -1,20 +1,40 @@
 #include <chrono>
 #include <iostream>
-class Ticker{
+class Ticker {
 public:
-    Ticker(): lastTime_(std::chrono::steady_clock::now()){}
-    void setNow(){ lastTime_ = std::chrono::steady_clock::now(); }
-    std::chrono::milliseconds elapsedTime(){
+    Ticker()
+        : lastTime_(std::chrono::steady_clock::now()) {}
+    void setNow() { lastTime_ = std::chrono::steady_clock::now(); }
+    std::chrono::milliseconds elapsedTime() {
         auto now = std::chrono::steady_clock::now();
         std::chrono::milliseconds ret = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastTime_);
         lastTime_ = now;
         return ret;
     }
+
 private:
     decltype(std::chrono::steady_clock::now()) lastTime_;
 };
 
+void printGettimeCost() {
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; i++) {
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> elapsed1 = end - start;
+    std::cout << "10000 times empty for cost: " << elapsed1.count() << "ms" << std::endl;
+    start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; i++) {
+        auto now = std::chrono::steady_clock::now();
+    }
+    end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> elapsed2 = end - start;
+    std::cout << "10000 times empty for cost: " << elapsed2.count() << "ms" << std::endl;
+    std::cout << "Get time cost: " << (elapsed2 - elapsed1).count() / 10000 * 1000000 << "ns" << std::endl;
+}
+
 int main() {
+    printGettimeCost();
     auto start = std::chrono::steady_clock::now();
     long long num{ (int64_t)10e9 };
     while (num--)
